9731a45d2ff776d571fe8749adc57f9f3193a64c
Shashi Bhushan <SBhushan@shashis-mac.local>
2015-12-26 22:52:42 +0530

    Initial Commit

---
+++ Demonstration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demonstration
+//
+//  Created by Shashi Bhushan on 12/26/15.
+//  Copyright © 2015 M.S. Bhushan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
